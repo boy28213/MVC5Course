1cf72bde43e9d9d7ad1a85fd0b8fab96dd26389f
Hsu <boy28213@gmail.com>
2017-05-07 09:51:35 +0800

    ViewModel 快速建立表單與接收表單資料, 利用XML快速註解

--- MVC5Course/Models/ViewModel/ProductLiteVM.cs
+++ MVC5Course/Models/ViewModel/ProductLiteVM.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace MVC5Course.Models.ViewModel
 {
+    /// <summary>
+    /// 給特定的View使用
+    /// </summary>
     public class ProductLiteVM
     {
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "請輸入商品名稱")]
         public string ProductName { get; set; }
+        [Required(ErrorMessage = "請輸入商品價格")]
         public Nullable<decimal> Price { get; set; }
+        [Required(ErrorMessage = "請輸入商品庫存")]
         public Nullable<decimal> Stock { get; set; }
     }
 }
