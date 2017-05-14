ebe828c7bcbcc4dcf9489f9ea0aab0a33c9f9fa0
Hsu <boy28213@gmail.com>
2017-05-14 11:16:26 +0800

    練習OutputCache 動作過濾器的使用方法

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -14,6 +14,7 @@ namespace MVC5Course.Controllers
     public class ProductsController : BaseController
     {
         // GET: Products
+        [OutputCache(Duration = 5, Location = System.Web.UI.OutputCacheLocation.ServerAndClient)]
         public ActionResult Index(bool Active = true)
         {
             var data = repo.GetProduct列表頁所有資料(Active, showAll: false);
